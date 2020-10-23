6a02464fc2946b13b428647b5f08e467374eb1b5
idhowardgj94 <idhowardgj94@users.noreply.github.com>
2020-10-23 16:42:58 +0000

    deploy: 93eacd1015dba3bc5d1d9cf75db5a0659c3bee34

---
+++ precache-manifest.a944503d66d7361dba50175869a990b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bf323806bcd9e1e0cc55f07374cc707",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c6da6e3e7562148a329",
+    "url": "/static/css/main.be218099.chunk.css"
+  },
+  {
+    "revision": "b68c22aa69526c4d53a5",
+    "url": "/static/js/2.489e66e2.chunk.js"
+  },
+  {
+    "revision": "4443239d0d5effbdcad6e195b6a252fb",
+    "url": "/static/js/2.489e66e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c6da6e3e7562148a329",
+    "url": "/static/js/main.7ce0ef9a.chunk.js"
+  },
+  {
+    "revision": "cb680665bccb0ad974a2",
+    "url": "/static/js/runtime-main.258cc42f.js"
+  },
+  {
+    "revision": "e9226429c4b0abd21f8f8535ba762eef",
+    "url": "/static/media/sample.e9226429.jpg"
+  }
+]);
